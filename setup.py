d9faf8af04a04605d5c3b37c413a0414046e0db1
im-rodrigo <rodrigopala91@gmail.com>
2014-12-19 07:24:47 -0800

    change to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='eatiht',
-    version='0.0.7',
+    version='0.0.8',
     description='A package and script for extracting article text in html documents.',
     keywords='extract extracted extracting extraction filter filtered filtering remove removed removing removal text textbody body content contents sentence sentences word words boilerplate boilerpipe delete tag tags opening closing main article html hypertext Rodrigo Palacios rodrigo palacios im-rodrigo im_rodrigo',
     url='https://github.com/im-rodrigo/eatiht',
